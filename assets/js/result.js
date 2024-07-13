1237b3ddb87912ef6c6959df5fe70d482670059c
taewoo <bighelper6440@gmail.com>
2024-07-12 20:26:44 -0400

    Movie Poster display codes added

--- assets/js/result.js
+++ assets/js/result.js
@@ -52,10 +52,28 @@ function getFilmDetails(id) {
       .then(data => {
         console.log(data);
         // Call the display results function here
+        displayDetailedResult(data);
       })
       .catch(error => {
         console.error('There was a problem with your fetch operation:', error);
       });
+}
+
+// Display Detailed Result fetched --> Testing
+function displayDetailedResult(data) {
+    // Just displaying a poster from a fixed source, only...^^. 
+    // Once Cole finishes the display function, it will be updated.
+    // *** Create a new img element
+    const imgElement = document.createElement('img');
+    // *** Set the scr attribute to the URL of the image
+    // imgElement.src = "https://m.media-amazon.com/images/M/MV5BMDQwOWQ0OGItNjJjYi00YzVjLTk4NmUtYzQ0NzUwZGY3NDZjXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_SX300.jpg";
+    imgElement.src = data.imageSet.verticalPoster.w360;
+    // *** Get the container element where you want to display the image
+    const container = document.getElementById('movie-poster');
+    // *** Append the img element to the container
+    container.appendChild(imgElement);
+
+    // Display detailed contents 
 }
 
 // Waits until the page is fully loaded
